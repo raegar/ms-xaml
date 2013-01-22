a17bfed79405226e6c780c50c5b14caccdaa21d1
raegar <jamie.myland@gmail.com>
2013-01-22 21:02:47 +0000

    Added Unit Test project

---
+++ HelloWorld/UnitTestsHelloWorld/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using HelloWorld;
+
+
+namespace UnitTestsHelloWorld
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
